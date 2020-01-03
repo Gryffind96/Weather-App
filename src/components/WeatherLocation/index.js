ab93134d46f2c67b79d1ed89a3859fd7fc38bf20
ebustillo <ebustillo@pop-os.localdomain>
2020-01-03 17:36:27 -0500

    general updates to handle double destructuring and component class with states

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,13 +1,56 @@
-import React from 'react';
+import React,{Component} from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
 
 import './styles.css';
-const WeatherLocation = ()=>(
-    <div className="weatherLocationCont">
-        <Location city={"Barcelona"} ></Location>
-        <WeatherData></WeatherData>
-    </div>
-);
+import {
+    SUN,
+    WINDY
+} from '../../constants/weathers';
+const data = {
+    temperature: 5,
+    weatherState:SUN,
+    humidity:10,
+    wind: '10 m/s'
+}
+
+const data2 = {
+    temperature: 15,
+    weatherState:WINDY,
+    humidity:20,
+    wind: '45 m/s'
+}
+
+class WeatherLocation extends Component{
+
+    constructor(){
+        super();
+        this.state = {
+            city: 'Buenos Aires',
+            data:data,
+        };
+    }
+
+    handleUpdateClick = ()=>{
+        console.log('actualizado');
+        this.setState({
+            city:'Barcelona',
+            data:data2
+        });
+    }
+
+    render(){
+
+        const {city,data} = this.state;
+
+        return (
+            <div className="weatherLocationCont">
+                <Location city={city} ></Location>
+                <WeatherData data={data}></WeatherData>
+                <button onClick={this.handleUpdateClick}>Actualizar</button>
+            </div>
+        );
+    }
+}
 
 export default WeatherLocation;
